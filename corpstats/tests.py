e8ddac711ad1cf07fe914f806a5203bd6def20be
Aaron Kable <aaronkable@gmail.com>
2019-12-07 01:46:05 +0000

    more tests stuff

--- corpstats/tests.py
+++ corpstats/tests.py
@@ -3,7 +3,7 @@ from unittest import mock
 from django.test import TestCase
 from django.utils.timezone import now
 from allianceauth.tests.auth_utils import AuthUtils
-from .models import CorpStats, CorpMember
+from .models import CorpStat, CorpMember
 from allianceauth.eveonline.models import EveCorporationInfo, EveAllianceInfo, EveCharacter
 from esi.models import Token
 from esi.errors import TokenError
@@ -21,7 +21,7 @@ class CorpStatsManagerTestCase(TestCase):
         cls.alliance = EveAllianceInfo.objects.create(alliance_id='3', alliance_name='test alliance', alliance_ticker='TEST', executor_corp_id='2')
         cls.corp = EveCorporationInfo.objects.create(corporation_id='2', corporation_name='test corp', corporation_ticker='TEST', alliance_id=3, member_count=1)
         cls.token = Token.objects.create(user=cls.user, access_token='a', character_id='1', character_name='test character', character_owner_hash='z')
-        cls.corpstats = CorpStats.objects.create(corp=cls.corp, token=cls.token)
+        cls.corpstats = CorpStat.objects.create(corp=cls.corp, token=cls.token)
         cls.view_corp_permission = Permission.objects.get_by_natural_key('view_corp_corpstats', 'corputils', 'corpstats')
         cls.view_alliance_permission = Permission.objects.get_by_natural_key('view_alliance_corpstats', 'corputils', 'corpstats')
         cls.view_state_permission = Permission.objects.get_by_natural_key('view_state_corpstats', 'corputils', 'corpstats')
@@ -34,50 +34,50 @@ class CorpStatsManagerTestCase(TestCase):
 
     def test_visible_superuser(self):
         self.user.is_superuser = True
-        cs = CorpStats.objects.visible_to(self.user)
+        cs = CorpStat.objects.visible_to(self.user)
         self.assertIn(self.corpstats, cs)
 
     def test_visible_corporation(self):
         self.user.user_permissions.add(self.view_corp_permission)
-        cs = CorpStats.objects.visible_to(self.user)
+        cs = CorpStat.objects.visible_to(self.user)
         self.assertIn(self.corpstats, cs)
 
     def test_visible_alliance(self):
         self.user.user_permissions.add(self.view_alliance_permission)
-        cs = CorpStats.objects.visible_to(self.user)
+        cs = CorpStat.objects.visible_to(self.user)
         self.assertIn(self.corpstats, cs)
 
     def test_visible_state_corp_member(self):
         self.state.member_corporations.add(self.corp)
         self.user.user_permissions.add(self.view_state_permission)
-        cs = CorpStats.objects.visible_to(self.user)
+        cs = CorpStat.objects.visible_to(self.user)
         self.assertIn(self.corpstats, cs)
 
     def test_visible_state_alliance_member(self):
         self.state.member_alliances.add(self.alliance)
         self.user.user_permissions.add(self.view_state_permission)
-        cs = CorpStats.objects.visible_to(self.user)
+        cs = CorpStat.objects.visible_to(self.user)
         self.assertIn(self.corpstats, cs)
 
     def test_visible_alliances(self):
         user = User.objects.get(pk=self.user.pk)
         user.user_permissions.add(self.view_corp_permission)
-        alliances = CorpStats.objects.alliances_visible_to(user)
+        alliances = CorpStat.objects.alliances_visible_to(user)
         self.assertEquals(len(alliances), 0)
 
         user.user_permissions.add(self.view_alliance_permission)
         user = User.objects.get(pk=self.user.pk)  # permissions cache is only cleared when retrieved fresh from db
-        alliances = CorpStats.objects.alliances_visible_to(user)
+        alliances = CorpStat.objects.alliances_visible_to(user)
         self.assertIn('3', alliances)
 
         user.user_permissions.clear()
         user = User.objects.get(pk=self.user.pk)
-        alliances = CorpStats.objects.alliances_visible_to(user)
+        alliances = CorpStat.objects.alliances_visible_to(user)
         self.assertEquals(len(alliances), 0)
 
         user.user_permissions.add(self.view_state_permission)
         user = User.objects.get(pk=self.user.pk)
-        alliances = CorpStats.objects.alliances_visible_to(user)
+        alliances = CorpStat.objects.alliances_visible_to(user)
         self.assertIn('3', alliances)
 
 
@@ -94,7 +94,7 @@ class CorpStatsUpdateTestCase(TestCase):
                                                     corporation_ticker='TEST')
 
     def setUp(self):
-        self.corpstats = CorpStats.objects.get_or_create(token=self.token, corp=self.corp)[0]
+        self.corpstats = CorpStat.objects.get_or_create(token=self.token, corp=self.corp)[0]
 
     @mock.patch('esi.clients.SwaggerClient')
     def test_update_member_relations(self, SwaggerClient):
@@ -170,7 +170,7 @@ class CorpStatsUpdateTestCase(TestCase):
         SwaggerClient.from_spec.return_value.Character.get_characters_character_id.return_value.result.return_value = {'corporation_id': 2}
         SwaggerClient.from_spec.return_value.Corporation.get_corporations_corporation_id_membertracking.return_value.result.side_effect = TokenError()
         self.corpstats.update()
-        self.assertFalse(CorpStats.objects.filter(corp=self.corp).exists())
+        self.assertFalse(CorpStat.objects.filter(corp=self.corp).exists())
         self.assertTrue(notify.called)
 
     @mock.patch('allianceauth.corputils.models.notify')
@@ -179,7 +179,7 @@ class CorpStatsUpdateTestCase(TestCase):
         SwaggerClient.from_spec.return_value.Character.get_characters_character_id.return_value.result.return_value = {'corporation_id': 2}
         SwaggerClient.from_spec.return_value.Corporation.get_corporations_corporation_id_membertracking.return_value.result.side_effect = HTTPForbidden(mock.Mock())
         self.corpstats.update()
-        self.assertFalse(CorpStats.objects.filter(corp=self.corp).exists())
+        self.assertFalse(CorpStat.objects.filter(corp=self.corp).exists())
         self.assertTrue(notify.called)
 
     @mock.patch('allianceauth.corputils.models.notify')
@@ -187,7 +187,7 @@ class CorpStatsUpdateTestCase(TestCase):
     def test_update_token_character_corp_changed(self, SwaggerClient, notify):
         SwaggerClient.from_spec.return_value.Character.get_characters_character_id.return_value.result.return_value = {'corporation_id': 3}
         self.corpstats.update()
-        self.assertFalse(CorpStats.objects.filter(corp=self.corp).exists())
+        self.assertFalse(CorpStat.objects.filter(corp=self.corp).exists())
         self.assertTrue(notify.called)
 
 
@@ -195,67 +195,15 @@ class CorpStatsPropertiesTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = AuthUtils.create_user('test')
-        AuthUtils.add_main_character(cls.user, 'test character', '1', corp_id='2', corp_name='test_corp', corp_ticker='TEST', alliance_id='3', alliance_name='TEST')
+        AuthUtils.add_main_character(cls.user, 'test character', '1', corp_id='2', corp_name='test_corp', corp_ticker='TEST', alliance_name='TEST')
         cls.user.profile.refresh_from_db()
         cls.token = Token.objects.create(user=cls.user, access_token='a', character_id='1', character_name='test character', character_owner_hash='z')
-        cls.corp = EveCorporationInfo.objects.create(corporation_id='2', corporation_name='test corp', corporation_ticker='TEST', member_count=1, alliance_id='3')
-        cls.corpstats = CorpStats.objects.create(token=cls.token, corp=cls.corp)
+        cls.corp = EveCorporationInfo.objects.create(corporation_id='2', corporation_name='test corp', corporation_ticker='TEST', member_count=1)
+        cls.corpstats = CorpStat.objects.create(token=cls.token, corp=cls.corp)
         cls.character = EveCharacter.objects.create(character_name='another test character', character_id='4', corporation_id='2', corporation_name='test corp', corporation_ticker='TEST')
         AuthUtils.disconnect_signals()
         CharacterOwnership.objects.create(character=cls.character, user=cls.user, owner_hash='a')
         AuthUtils.connect_signals()
-
-    def test_member_count(self):
-        member = CorpMember.objects.create(character_id='2', character_name='old test character', corpstats=self.corpstats,
-                                  location_id=1, location_name='test', ship_type_id=1, ship_type_name='test',
-                                  logoff_date=now(), logon_date=now(), start_date=now())
-        self.assertEqual(self.corpstats.member_count, 1)
-        member.delete()
-        self.assertEqual(self.corpstats.member_count, 0)
-
-    def test_user_count(self):
-        member = CorpMember.objects.create(character_id='4', character_name='another test character', corpstats=self.corpstats,
-                                  location_id=1, location_name='test', ship_type_id=1, ship_type_name='test',
-                                  logoff_date=now(), logon_date=now(), start_date=now(), main_character=self.character)
-        self.assertEqual(self.corpstats.user_count, 1)
-        member.main_character = None
-        member.save()
-        self.corpstats.refresh_from_db()
-        self.assertEqual(self.corpstats.user_count, 0)
-
-    def test_registered_members(self):
-        member = CorpMember.objects.create(character_id='4', character_name='another test character', corpstats=self.corpstats,
-                                  location_id=1, location_name='test', ship_type_id=1, ship_type_name='test',
-                                  logoff_date=now(), logon_date=now(), start_date=now(), main_character=self.character, registered=True)
-        self.assertIn(member, self.corpstats.registered_members)
-        self.assertEquals(self.corpstats.registered_member_count, 1)
-        self.assertNotIn(member, self.corpstats.unregistered_members)
-        self.assertEquals(self.corpstats.unregistered_member_count, 0)
-
-        member.registered = False
-        member.save()
-        self.corpstats.refresh_from_db()
-
-        self.assertNotIn(member, self.corpstats.registered_members)
-        self.assertEquals(self.corpstats.registered_member_count, 0)
-        self.assertIn(member, self.corpstats.unregistered_members)
-        self.assertEquals(self.corpstats.unregistered_member_count, 1)
-
-    def test_mains(self):
-        # test when is a main
-        member = CorpMember.objects.create(character_id='4', character_name='another test character', corpstats=self.corpstats,
-                                  location_id=1, location_name='test', ship_type_id=1, ship_type_name='test',
-                                  logoff_date=now(), logon_date=now(), start_date=now(), main_character=self.character, is_main=True)
-        self.assertIn(member, self.corpstats.mains)
-        self.assertEqual(self.corpstats.main_count, 1)
-
-        # test when is not a main
-        member.is_main = False
-        member.save()
-        self.corpstats.refresh_from_db()
-
-        self.assertNotIn(member, self.corpstats.mains)
-        self.assertEqual(self.corpstats.main_count, 0)
 
     def test_logos(self):
         self.assertEqual(self.corpstats.corp_logo(size=128), 'https://image.eveonline.com/Corporation/2_128.png')
@@ -270,7 +218,7 @@ class CorpMemberTestCase(TestCase):
         cls.user.profile.refresh_from_db()
         cls.token = Token.objects.create(user=cls.user, access_token='a', character_id='1', character_name='test character', character_owner_hash='a')
         cls.corp = EveCorporationInfo.objects.create(corporation_id='2', corporation_name='test corp', corporation_ticker='TEST', member_count=1)
-        cls.corpstats = CorpStats.objects.create(token=cls.token, corp=cls.corp)
+        cls.corpstats = CorpStat.objects.create(token=cls.token, corp=cls.corp)
         cls.member = CorpMember.objects.create(corpstats=cls.corpstats, character_id='2', character_name='other test character', location_id=1, location_name='test', ship_type_id=1, ship_type_name='test', logoff_date=now(), logon_date=now(), start_date=now())
 
     def test_portrait_url(self):
