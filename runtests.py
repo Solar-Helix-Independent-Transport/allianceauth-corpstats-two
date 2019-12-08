f50278f2b72971ff6199e827444ac1c982fff2c7
Aaron Kable <aaronkable@gmail.com>
2019-12-08 13:09:50 +0000

    Hopefully working tests?

--- runtests.py
+++ runtests.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-
+    print(os.getcwd())
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
