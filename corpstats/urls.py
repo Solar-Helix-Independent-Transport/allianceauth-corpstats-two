73d39f6228fa4b9893a390b302e7416ecee6a56f
Peter Pfeufer <info@ppfeufer.de>
2022-02-27 15:27:30 +0100

    FIX: ImportError: cannot import name 'url' from 'django.conf.urls'

--- corpstats/urls.py
+++ corpstats/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'corpstat'
 
 urlpatterns = [
-    url(r'^$', views.corpstat_view, name='view'),
-    url(r'^add/$', views.corpstats_add, name='add'),
-    #url(r'^alliance/(?P<alliance_id>(\d)+)/$', views.alliance_view, name='view_alliance'),
-    url(r'^overview/$', views.overview_view, name='view_all'),
-    url(r'^(?P<corp_id>(\d)*)/$', views.corpstat_view, name='view_corp'),
-    url(r'^(?P<corp_id>(\d)+)/update/$', views.corpstats_update, name='update'),
-    url(r'^(?P<corp_id>(\d)+)/export/$', views.export_corpstats, name='export'), # has no permissions
-    url(r'^search/$', views.corpstats_search, name='search'),
+    re_path(r'^$', views.corpstat_view, name='view'),
+    re_path(r'^add/$', views.corpstats_add, name='add'),
+    #re_path(r'^alliance/(?P<alliance_id>(\d)+)/$', views.alliance_view, name='view_alliance'),
+    re_path(r'^overview/$', views.overview_view, name='view_all'),
+    re_path(r'^(?P<corp_id>(\d)*)/$', views.corpstat_view, name='view_corp'),
+    re_path(r'^(?P<corp_id>(\d)+)/update/$', views.corpstats_update, name='update'),
+    re_path(r'^(?P<corp_id>(\d)+)/export/$', views.export_corpstats, name='export'), # has no permissions
+    re_path(r'^search/$', views.corpstats_search, name='search'),
     ]
